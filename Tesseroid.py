590f81b9fa626ff69e46255d581cd695a8ded1dc
Xianghang Zeng <zengxianghang@qq.com>
2022-03-31 19:57:27 +0800

    Add Tesseroid calss

---
+++ Tesseroid.py
@@ -0,0 +1,11 @@
+class Tesseroid:
+    def __init__(self, r_min, r_max, phi_min, phi_max, lambda_min, lambda_max,\
+        apparent_density, density_gradient):
+        self.r_min = r_min
+        self.r_max = r_max
+        self.phi_min = phi_min
+        self.phi_max = phi_max
+        self.lambda_min = lambda_min
+        self.lambda_max = lambda_max
+        self.apparent_density = apparent_density
+        self.denisty_gradient = density_gradient
